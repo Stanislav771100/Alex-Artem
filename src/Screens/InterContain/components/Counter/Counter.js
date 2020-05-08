8e2bbe18d3a0ad08ec044c01379417693b9191bb
Artemchyk <artemv.lizunkov@gmail.comm>
2020-05-08 18:14:21 +0300

    added wave

--- src/Screens/InterContain/components/Counter/Counter.js
+++ src/Screens/InterContain/components/Counter/Counter.js
@@ -7,13 +7,23 @@ class Counter extends Component {
   constructor(props) {
     super(props)
     this.state = {
-     
+     WaveCounter : 0,
+     EnemyStart : 0,
+     EnemyEnd : 50
     }
   }
   render() {
-
+const { WaveCounter, EnemyStart, EnemyEnd } = this.state
     return (
     <div className="main">
+
+      <div className="Wave">
+      <p>{WaveCounter}</p>
+      </div>
+
+      <div className="Enemy">
+      <p>{EnemyStart}/{EnemyEnd}</p>
+      </div>
 
     </div>
     );

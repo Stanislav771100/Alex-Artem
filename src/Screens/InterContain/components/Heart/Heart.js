7d770470d44a555d104ac0a1fc183d395f512bd4
Pepper918 <oleksiyv.lizunkov@gmail.com>
2020-05-08 18:15:24 +0300

    added Heart

--- src/Screens/InterContain/components/Heart/Heart.js
+++ src/Screens/InterContain/components/Heart/Heart.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Heart.css';
+import heart from "../../../../img/heart.png";
 
 
 
@@ -7,15 +8,21 @@ class Heart extends Component {
   constructor(props) {
     super(props)
     this.state = {
-     
+
+      heartStart: 0,
+      heartAnd: 20
     }
   }
   render() {
-
+    const { heartStart, heartAnd } = this.state;
     return (
-    <div className="mainHeart">
-
-    </div>
+      <div className="mainHeart">
+        
+          <img alt="heart" src={heart} />
+          <p>{heartStart} / {heartAnd}</p>
+        
+        
+      </div>
     );
   }
 }

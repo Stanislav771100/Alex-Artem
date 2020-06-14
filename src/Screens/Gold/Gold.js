239a98e9568f7f0ec8997ed683b877b4916024e3
Станислав Штанько <shtanislav.shtanko@gmail.com>
2020-06-14 13:48:27 +0300

    adding modal

--- src/Screens/Gold/Gold.js
+++ src/Screens/Gold/Gold.js
@@ -7,15 +7,39 @@ class Gold extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      gold: 80
+      gold: 80,
+      goldInput: 0    }
+  }
+  plusGold(variables) {
+    const { gold } = this.state
+    if (variables == 'mnoj') {
+      this.setState({gold: gold * 5})
+    } else
+
+    this.setState({gold: gold+5})
+  }
+  setGold(variable){
+    const { goldInput, gold } = this.state
+    let plusGold = parseInt(goldInput)
+    if (variable == 'Plus') {
+      this.setState({gold: gold + plusGold})
+    } else if ( variable == 'Minus') { 
+      this.setState({gold: gold - plusGold})
     }
   }
   render() {
-    const { gold } = this.state
+    const { gold, goldInput } = this.state
     return (
     <div className="gold-container">
-      <GrMoney style={{height: 50, width: 50}} />
-      <p>{gold}</p>
+      <GrMoney onClick={() => this.plusGold('mnoj')} style={{height: 50, width: 50}} />
+      <p onMouseEnter={() => this.plusGold()}>{gold}</p>
+      <input onChange={e => this.setState({goldInput: e.target.value})} placeholder="number" type="text"/>
+      <div onClick={() => this.setGold('Plus')} className="plusButton">
+        PLUS
+      </div>
+      <div onClick={() => this.setGold('Minus')} className="plusButton">
+        Minus
+      </div>
     </div>
     );
   }
--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,70 +1,76 @@
-import React, { Component } from 'react';
-import './Home.css';
-import Monsters from '../Monsters/Monstres'
-import Towers from '../Towers/Towers'
-import InterContain from '../InterContain/InterContain'
-import Gold from '../Gold/Gold';
+import React, { Component } from "react";
+import "./Home.css";
+import Monsters from "../Monsters/Monstres";
+import Towers from "../Towers/Towers";
+import InterContain from "../InterContain/InterContain";
+import Gold from "../Gold/Gold";
 
 class Home extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       monster: [
         {
-        hp : 100,
-        goldAfterDeath : 15,
-        name : 'elf',
-        img: '🏹'
-       },
-       {
-        hp : 200,
-        goldAfterDeath : 25,
-        name : 'spider',
-        img: '🕷️'
-       },
-      {
-        hp : 50,
-        goldAfterDeath : 10,
-        name : 'wasp',
-        img: '🐝'
-       },
+          hp: 100,
+          goldAfterDeath: 15,
+          name: "elf",
+          img: "🏹"
+        },
+        {
+          hp: 200,
+          goldAfterDeath: 25,
+          name: "spider",
+          img: "🕷️"
+        },
+        {
+          hp: 50,
+          goldAfterDeath: 10,
+          name: "wasp",
+          img: "🐝"
+        }
       ],
       tower: [
         {
           dm: 75,
           cost: 75,
-          name: 'Cannon',
-          img: '🗼'
+          name: "Cannon",
+          img: "🗼"
         },
-  
+
         {
-          dm : 100,
-          cost : 200,
-          name : 'Earth Tower',
-          img: '💣'
+          dm: 100,
+          cost: 200,
+          name: "Earth Tower",
+          img: "💣"
         },
-  
+
         {
-          dm : 50,
-          cost : 100,
-          name : 'Sky Tower',
-          img: '🚀'
-        },
+          dm: 50,
+          cost: 100,
+          name: "Sky Tower",
+          img: "🚀"
+        }
       ]
-    }
+    };
   }
   render() {
-    const { monster, tower } = this.state
+    const { monster, tower } = this.state;
 
     return (
-    <div className="container">
-      <div className="left-bar">
+      <div className="container">
+        <div className="modal">
+          <input placeholder='username' type="text" className="formRegister" />
+          <input placeholder='email' type='email' className="formRegister" />
+          <input placeholder='password' type="password" className="formRegister" />
+          <div className="button">accept</div>
+        </div>
+        {/* <div className="left-bar">
       <Towers towers={tower} />
       <Gold />
       </div>
       <Monsters monsters={monster}/>
-      <InterContain />
-    </div>
+      <InterContain /> */}
+      </div>
     );
   }
 }
